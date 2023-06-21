f9d7eda53cb65d9f3a23f3656a4bd8d491ac51b2
StianMM <64170108+StianMM@users.noreply.github.com>
2023-06-22 01:27:04 +0200

    Added a dark/light mode toggle button, with functionality

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -3,13 +3,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TriviaGame;
 using TriviaGame.Pages.Gameplay;
+using TriviaGame.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<TriviaScore>();
-builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
